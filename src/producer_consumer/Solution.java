34683cd95a23b78328d2a00aa152551ded0042c7
lusuon <542343357@qq.com>
2018-12-19 00:00:59 +0800

    update

--- src/producer_consumer/Solution.java
+++ src/producer_consumer/Solution.java
@@ -1,156 +1,112 @@
 package producer_consumer;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ExecutorService;
 
-import producer_consumer.Solution.WareHouse;
-import sun.invoke.empty.Empty;
 
+/**
+ * 多生产者、消费者问题
+ */
 public class Solution{
-    public final static int MUTEX = 1;
-    public final static int EMPTY = 1;
-    public final static int FULL = 0;
+    // 创建仓库类，管理资源
+    static Warehouse wareHouse = new Warehouse();
 
-    public static class WareHouse implements Runnable{
-        int products = 0;
-        final int maxCapacity = 100;
-        Semaphore full;
-        Semaphore mutex;
-        Semaphore empty;
-        
-        /**
-         * @return the products
-         */
-        public int getProducts() {
-            return products;
-        }
-        /**
-         * @param products the products to set
-         */
-        public void setProducts(int products) {
-            this.products = products;
-        }
-
-
-
+    // 生产者
+    static class Producer implements Runnable {
+        static int num = 1;
         @Override
         public void run() {
-            while(true){
-                synchronized (this){
-                    while (true) {
-                        
-                        
-
-                    }
+            int n = num++;//每个生产者的产量与生成顺序成正比
+            while (true) {
+                try {
+                    wareHouse.produce(n);
+                    System.out.println("Produce " + n+" products.");
+                    // 休息1秒
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                
             }
         }
-
-        
     }
-
+    // 消费者
+    static class Consumer implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("Consumed " + wareHouse.consume() +" products.");
+                    // 休息1秒
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     /**
-     * 生产者类，执行时，生产给定数量的产品
+     * 仓库 
+     *
      */
-    public static class Producer implements Runnable {
-        Semaphore empty;
-        Semaphore mutex;
-        Semaphore full;
-        int speed;
-        WareHouse warehouse;
+    static class Warehouse {
+        // 非满锁，最多允许10个生产者
+        final Semaphore notFull = new Semaphore(10);
+        // 非空锁  
+        final Semaphore notEmpty = new Semaphore(0);
+        // 核心锁  
+        final Semaphore mutex = new Semaphore(1);
+        // 库存容量  
+        final Object[] items = new Object[10];
+        int producePointer, consumePointer, count;
 
-        public Producer(int speed, WareHouse warehouse) {
-            this.speed = speed;
-            this.warehouse = warehouse;
-        }
-            
-        public synchronized void product(int x,WareHouse wareHouse) {
-            while (true)  {
-                if(mutex.tryAcquire()){
-                    
-                }else{
-
-                }
-                if(products == maxCapacity){
-                    System.out.println("The warehouse is full now,producer stop producing.");
-                }
-                sleep(1000);
-                
-
-                //produce an item
-                wait (empty);
-                wait (mutex);
-                //add the item to the  buffer
-                signal (mutex);
-                signal (full);
+        public void produce(Object x) throws InterruptedException {
+            // 保证非满，无法得到就阻塞
+            notFull.acquire();
+            // 保证操作临界区时不发生冲突
+            mutex.acquire();
+            try {
+                // 增加库存  
+                items[producePointer] = x;
+                producePointer++;
+                //实现循环
+                if (producePointer == items.length) producePointer = 0;
+                count++;
+            } finally {
+                // 退出临界区
+                mutex.release();
+                // 释放非空信号量，允许消费
+                notEmpty.release();
             }
         }
 
-                @Override
-                public void run() {
-                    warehouse.product(speed);
-                }
+        public Object consume() throws InterruptedException {
+            // 保证非空，如果无法得到就阻塞
+            notEmpty.acquire();
+            // 保证操作临界区时不发生冲突
+            mutex.acquire();
+            try {
+                // 减少库存  
+                Object x = items[consumePointer];
+                consumePointer++;
+                //实现循环队列，到最后时回到数组头
+                if (consumePointer == items.length) consumePointer = 0;
+                count--;
+                return x;
+            } finally {
+                // 退出核心区  
+                mutex.release();
+                // 释放非满的信号量，允许生产
+                notFull.release();
             }
-
-        /**
-         * 生产者类，执行时，生产给定数量的产品
-         */
-    public static class Customer implements Runnable {
-        int speed;
-        WareHouse warehouse;
-
-        public Customer(int speed, WareHouse warehouse) {
-            this.speed = speed;
-            this.warehouse = warehouse;
-        }
-
-        /**
-             * 消费x个产品
-             * @param x
-             */
-            public synchronized void consume(int x) {
-                Semaphore full;
-                Semaphore mutex;
-                Semaphore empty;
-                while (num - x < 0) {
-                    try {
-                        if(products == 0){
-                            System.out.println("The warehouse is empty now,consumer stop consuming.");
-                        }
-                        System.out.println("容量不足，等待生产~");
-                        wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                num = num - x;
-                System.out.println("已消费~");
-                notifyAll();
-                while (true) {
-                    wait (full);
-                    wait (mutex);
-                    //  remove an item from  buffer
-                    signal (mutex);
-                    signal (empty);
-                    //  consume the removed item
-                }
-            }
-        @Override
-        public void run() {
-            warehouse.consume(speed);
         }
     }
-    
+
     public static void main(String[] args) {
-        WareHouse warehouse = new WareHouse();
-
-        Semaphore mutex = new Semaphore(MUTEX);// 1个互斥锁
-        Semaphore empty = new Semaphore(EMPTY);// 空信号量
-        Semaphore full = new Semaphore(FULL);// 满信号量
-
-        Thread barber = new Thread(new BarberShop.Barber(mutex, empty));//创建理发师线程
-            barber.start();
+        for (int i = 0; i <= 5; i++) {
+            // 创建生产者、消费者各5个
+            new Thread(new Producer()).start();
+            new Thread(new Consumer()).start();
         }
+    }
 }
