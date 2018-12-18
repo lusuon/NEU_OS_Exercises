7fdf2f3b6900256ad8f0630600ff4d63de412149
lusuon <542343357@qq.com>
2018-12-19 00:14:47 +0800

    sync

---
+++ src/memory_management/Solution.java
@@ -0,0 +1,12 @@
+package memory_management;
+
+public class Solution {
+    static class PageTable{
+        int pageNumber;
+        int symbol;
+        int blockNumber;
+        int location;
+
+
+    }
+}
