762d2035055edb062bb189a5d45770aa13ddf3f3
lusuon <542343357@qq.com>
2018-12-20 00:17:59 +0800

    update

--- src/CPU_schedule/Solution_slice.java
+++ src/CPU_schedule/Solution_slice.java
@@ -4,116 +4,63 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
-import com.sun.tools.javac.code.Attribute.Array;
 
 public class Solution_slice {
     /**
      * Slice solution
      */
 
-    public static void main(String[] args){
-        PCB P5=new PCB("P5",null,2,0,0,1,"ready");
-        PCB P4=new PCB("P4",P5,2,0,0,1,"ready");
-        PCB P3=new PCB("P3",P4,2,0,0,1,"ready");
-        PCB P2=new PCB("P2",P3,2,0,0,1,"ready");
-        PCB P1=new PCB("P1",P2,2,0,0,1,"ready");
+    public static void main(String[] args) {
+        PCB P5 = new PCB("P5", null, 2, 0, 0, "ready");
+        PCB P4 = new PCB("P4", P5, 2, 0, 0, "ready");
+        PCB P3 = new PCB("P3", P4, 2, 0, 0, "ready");
+        PCB P2 = new PCB("P2", P3, 2, 0, 0, "ready");
+        PCB P1 = new PCB("P1", P2, 2, 0, 0, "ready");
         P5.pointer = P1;
-
         PCB ready = P1;
-
-        while(ready.pointer!=ready){
-            if(ready.needTime!=0){
-                ready.needTime--;
-            ready.CPUTime++;
-        
-            }
-        }
-
-        
-        for (PCB process:PCBs){
-            tasks.add(process);
-        }
-        
-
-        while(tasks.isEmpty()){
-            PCB[] order_PCBs = new PCB[tasks.size()]; 
-            PCB working = null;
-            current = working;
-            int count = 0;
-            //数组内为此时为已排序好的
-            while(!tasks.isEmpty()){
-                order_PCBs[count] = tasks.poll();
-                if(count>1) order_PCBs[count-1].pointer = order_PCBs[count];
-                else working = order_PCBs[0];
-                count++;
-            }
-            order_PCBs[count-1].pointer = order_PCBs[0];
-            //完成构造循环队列
-            
-            while(first.pointer!=first){
-                //遍历链表，确定first
-                if()
-                first.needTime--;
-                first.CPUTime++;
-                    
-                
-            }
-
-            
-            
-            
-            current.status="working";
-            
-            
-            current.needTime--;
-            current.CPUTime++;
+        PCB finish = null;
+        PCB current = ready;
+        int size = 5;
 
 
+        while(size!=1){
 
 
-            for (PCB process: tasks) {
-                if (process.priority == current.priority){
-                    process.CPUTime++;
-                    process.wait++;
-                }
-            }
-            current.priority--;
-
-
-            if(current.needTime!=0){
-                tasks.add(current);
+            if(current.needTime != 0){
+                current.status="working";
+                current.needTime--;
+                current.CPUTime++;
+                current.status="ready";
             }else{
-                current.status = "finish";
+                current.status="finish";
+                size--;
             }
-
-            System.out.println("CPU TIME:"+count);
-            System.out.print("NAME\tCPUTIME\tNEEDTIME\tPRIORITY\tSTATE\n");
-
-            for (PCB process:PCBs) {
-                System.out.println(process.toString());
+            if(current.status.equals("finish")){
+                System.out.println("Encounter a finished process.");
+                PCB next = current.pointer.pointer;
+                if(finish==null) {
+                    finish = current.pointer;
+                    finish.pointer =null;
+                }else{
+                    current.pointer.pointer = finish;
+                    finish = current.pointer;
+                }
+                current.pointer = next;
             }
-            if (!current.status.equals("finish")) current.status="ready";
-
-            //rebuild the heap
-            ArrayList<PCB> temp = new ArrayList<>();
-            while(!tasks.isEmpty()){
-                PCB pop = tasks.poll();
-                temp.add(pop);
-            }
-            for (PCB process:temp) {
-                tasks.add(process);
-            }
+            current = current.pointer;
 
         }
-        System.out.print("NAME\tRoundTime\tWaitingTime\n");
-        for (PCB process:PCBs) {
-            System.out.println(process.wait);
-        }
 
+
+
+        while(finish!=null){
+            System.out.print(finish.name+"\t");
+            finish = finish.pointer;
+        }
     }
 
 
-    public static class PCB implements Comparable{
+    public static class PCB {
         String name;
         PCB pointer;
         int needTime;
@@ -122,7 +69,7 @@ public class Solution_slice {
         String status;
 
 
-        public PCB(String name, PCB pointer, int needTime,int CPUTime,int wait , int priority, String status) {
+        public PCB(String name, PCB pointer, int needTime,int CPUTime,int wait, String status) {
             this.name = name;
             this.pointer = pointer;
             this.needTime = needTime;
@@ -133,15 +80,6 @@ public class Solution_slice {
 
 
 
-        @Override
-        public int compareTo(Object o) {
-            PCB another = (PCB)o;
-            if (another.priority == this.priority){
-                return Integer.valueOf(another.name.split("P")[1])-Integer.valueOf(name.split("P")[1]);
-            }else{
-                return another.priority - this.priority;
-            }
-        }
 
         @Override
         public String toString() {
