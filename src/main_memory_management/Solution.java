eb8565ce12b2b861caf83dcc792a93d9ec0f84dc
lusuon <542343357@qq.com>
2018-12-22 15:05:35 +0800

    update

--- src/main_memory_management/Solution.java
+++ src/main_memory_management/Solution.java
@@ -1,29 +1,50 @@
 package main_memory_management;
 
-public class solution{
-    int blockLength = 128;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
-    private class PTEntry{
+public class Solution{
+    int count = 0;//模拟计时
+    int blockLength = 128;//内存块长度
+
+    private static ArrayList<PTEntry> PT = new ArrayList<>();//模拟页表
+
+    private static class PTEntry implements Comparable{
         int pageNum;
         int symbol;
         int memBlock;
         String location;
         int modify;
+        int time;
+
+        public PTEntry(int pageNum, int symbol, int memBlock, int modify, String location, int time) {
+            this.pageNum = pageNum;
+            this.symbol = symbol;
+            this.memBlock = memBlock;
+            this.location = location;
+            this.modify = modify;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            PTEntry another =(PTEntry) o;
+            return time - another.time;
+        }
     }
-    private ArrayList<PTEntry> PT = new ArrayList<>();
 
     private String convert(int pageN,int unit){
         String absAddr = "*";
-        for (PTEntry ptE: PT) {
-            if(ptE.pageNum == pageN){
-                if(ptE.symbol == 1){
-                    adsAddr = Integer.toString(ptE.memBlock)+"\t"+Integer.toString(blockLength+unit); 
+        for (PTEntry e: PT) {
+            if(e.pageNum == pageN){
+                if(e.symbol == 1){
+                    absAddr = Integer.toString(e.memBlock)+"\t"+Integer.toString(blockLength+unit);
                 }else{
-                    System.out.println("Page not in memory,page number is:"+ptE.pageNum);
-                    adsAddr += ptE.pageNum;
+                    System.out.println("Page not in memory,page number is:"+e.pageNum);
+                    absAddr += e.pageNum;
                 }
                 // 如果找到，则从此退出
-                return "";
+                return absAddr;
             }
         }
         //没找到
@@ -32,12 +53,45 @@ public class solution{
     }
 
     private void handle(String convertResult){
-        
+        int pageNum = Integer.valueOf(convertResult.split("\\*")[1]);
+        PriorityQueue<PTEntry> queue = new PriorityQueue<>();//用于实现FIFO的队列
+        PTEntry newest = null;
+
+        for (PTEntry e:PT){
+            if(e.pageNum!=pageNum) queue.add(e);
+            else newest = e;
+        }
+        PTEntry oldest = queue.poll();
+
+        //修改页表
+        newest.time = count;
+
+
+        if(oldest.modify == 1) System.out.println("Move out:page "+oldest.pageNum);
+        System.out.println("Move in:page "+pageNum);
     }
 
 
     public static void main(String args[]){
-        
-    }
+        Solution s = new Solution();
+        PTEntry p0 = new PTEntry(0,1,5,0,"011",0);
+        PTEntry p1 = new PTEntry(1,1,8,0,"012",0);
+        PTEntry p2 = new PTEntry(2,1,9,0,"013",0);
+        PTEntry p3 = new PTEntry(3,1,1,0,"021",0);
+        PTEntry p4 = new PTEntry(4,0,-1,0,"022",0);
+        PTEntry p5 = new PTEntry(5,0,-1,0,"023",0);
+        PTEntry p6 = new PTEntry(6,0,-1,0,"121",0);
 
+        PT.add(p0);
+        PT.add(p1);
+        PT.add(p2);
+        PT.add(p3);
+        PT.add(p4);
+        PT.add(p5);
+        PT.add(p6);
+
+        System.out.println(s.convert(0,24));
+        s.handle(s.convert(4,24));
+
+    }
 }
